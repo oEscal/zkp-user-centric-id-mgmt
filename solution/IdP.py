f4a47d7387f1b6060f3688e5e2580d05d119ce2c
oEscal <pedroescaleira@hotmail.com>
2021-05-15 21:05:09 +0100

    some work done, but nothing working

--- solution/IdP.py
+++ solution/IdP.py
@@ -1,22 +1,35 @@
+import base64
+
 import cherrypy
 
-class IdP(object):
+from onelogin.saml2.utils import OneLogin_Saml2_Utils
 
-	def static_contents( self, path ):
-		return open( 'static/' + path, 'r' ).read()
+from saml2 import create_class_from_xml_string, SamlBase
+from saml2.samlp import AuthnRequest
+
+
+class IdP(object):
+	@staticmethod
+	def static_contents(path):
+		return open(f"static/{path}", 'r').read()
 
 	@cherrypy.expose
 	def index(self):
-            raise cherrypy.HTTPRedirect( '/authenticate', status=307 )
+		raise cherrypy.HTTPRedirect('/authenticate', status=307)
 
 	@cherrypy.expose
-	def authenticate(self):
-            return self.static_contents( 'authenticate.html' )
+	def authenticate(self, **kwargs):
+		saml_request: AuthnRequest = \
+			create_class_from_xml_string(AuthnRequest,
+			                             OneLogin_Saml2_Utils.decode_base64_and_inflate(kwargs['SAMLRequest']))
+		cherrypy.response.cookie['id'] = saml_request.id
+		return cherrypy.HTTPRedirect('zkp_helper_app')
 
 	@cherrypy.expose
 	def credentials(self, username):
-            raise cherrypy.HTTPRedirect( "http://localhost:8081/identity?username=" + username, 307 )
+		raise cherrypy.HTTPRedirect("http://localhost:8081/identity?username=" + username, 307)
 
-cherrypy.config.update( {'server.socket_host': '127.0.0.1',
-                         'server.socket_port': 8082 } )
-cherrypy.quickstart( IdP() )
+
+cherrypy.config.update({'server.socket_host': '127.0.0.1',
+                        'server.socket_port': 8082})
+cherrypy.quickstart(IdP())
--- solution/SP.py
+++ solution/SP.py
@@ -1,43 +1,66 @@
-import os
 from pathlib import Path
-import cherrypy
+import os
 import base64
 import hashlib
 
+import cherrypy
+from onelogin.saml2.auth import OneLogin_Saml2_Auth
+from onelogin.saml2.utils import OneLogin_Saml2_Utils
+
+
+saml_settings = {
+	'idp': {
+		'entityId': "http://127.0.0.1:8082",
+		'singleSignOnService': {
+			'url': "http://127.0.0.1:8082/authenticate",
+			'binding': "url:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"
+		}
+	},
+	'sp': {
+		'entityId': "http://127.0.0.1:8081",
+		'assertionConsumerService': {
+			'url': "http://127.0.0.1:8081/identity",
+			'binding': "url:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"
+		}
+	}
+}
+
+
 class SP(object):
+	@staticmethod
+	def random_name() -> str:
+		"""Creates a random name just for temporarility storing an uploded file
+		:return:
+		"""
+		return base64.urlsafe_b64encode(os.urandom(15)).decode('utf8')
 
-	# Creates a random name just for temporarility storing an uploded file
-	def random_name( self ):
-		return base64.urlsafe_b64encode( os.urandom( 15 ) ).decode( 'utf8' )
+	@staticmethod
+	def static_page(path: str):
+		"""Reads a static HTML page
+		:param path:
+		:return:
+		"""
+		return open(f"static/{path}", 'r').read()
 
-	# Reads a static HTML page
-	def static_page( self, path ):
-		return open( 'static/' + path, 'r' ).read()
-
-	# Checks if the request comes with an account cookie
-	# This code is unsafe (the cookie can be forged!)
-	def get_account( self, redirect ):
-		cookies = cherrypy.request.cookie
-		if not cookies:
-			if redirect:
-				raise cherrypy.HTTPRedirect( '/login', status=307 )
-			else:
-			    return False
-		username = cookies['username'].value
-		self.set_cookie( username ) # for keeping the session alive
-		return username
-
-	# Create a session cookie (insecure, can be forged)
-	# The validity is short by design, to force authentications
-	def set_cookie( self, username ):
+	@staticmethod
+	def set_cookie(username: str):
+		"""Create a session cookie (insecure, can be forged)
+		The validity is short by design, to force authentications
+		:param username:
+		:return:
+		"""
 		cookie = cherrypy.response.cookie
 		cookie['username'] = username
 		cookie['username']['path'] = '/'
 		cookie['username']['max-age'] = '20'
 		cookie['username']['version'] = '1'
 
-	# Present the account images and an upload form
-	def account_contents( self, account ):
+	@staticmethod
+	def account_contents(account: str) -> str:
+		"""Present the account images and an upload form
+		:param account:
+		:return:
+		"""
 		contents = '<html><body>'
 		contents += '<p>Upload a new image file</p>'
 		contents += '<form action="add" method="post" enctype="multipart/form-data">'
@@ -47,9 +70,9 @@ class SP(object):
 		contents += '<form action="add" method="post" enctype="multipart/form-data">'
 		contents += '<p>List of uploaded image file</sp>'
 		contents += '<table border=0><tr>'
-		
-		path = 'accounts/' + account
-		files = os.listdir( path )
+
+		path = f"accounts/{account}"
+		files = os.listdir(path)
 		count = 0
 		for f in files:
 			contents += '<td><img src="/img?name=' + f + '"></td>'
@@ -59,69 +82,125 @@ class SP(object):
 		contents += '</tr></body></html>'
 		return contents
 
-	# Root HTTP server method
+	@staticmethod
+	def prepare_auth_parameter(request):
+		return {
+			'http_host': request.local.name,
+			'script_name': request.path_info,
+			'server_port': request.local.port,
+			'get_data': request.params.copy(),
+			'post_data': request.params.copy()
+		}
+
+	def get_account(self, redirect):
+		"""Checks if the request comes with an account cookie
+		This code is unsafe (the cookie can be forged!)
+		:param redirect:
+		:return:
+		"""
+		cookies = cherrypy.request.cookie
+		if not cookies:
+			if redirect:
+				# raise cherrypy.HTTPRedirect('/login', status=307)
+				req = self.prepare_auth_parameter(cherrypy.request)
+				auth = OneLogin_Saml2_Auth(req, saml_settings)
+				# raise cherrypy.HTTPRedirect('/login', status=307)
+				raise cherrypy.HTTPRedirect(auth.login())
+				# auth.process_response()
+				# errors = auth.get_errors()
+				# if not errors:
+				# 	if auth.is_authenticated():
+				# 		print("Ola")
+				# 		print(auth)
+				# 	else:
+				# 		print("Adeus")
+				# else:
+				# 	print(errors)
+			else:
+				return False
+		username = cookies['username'].value
+		self.set_cookie(username)  # for keeping the session alive
+		return username
+
 	@cherrypy.expose
 	def index(self):
-		account = self.get_account( True )
+		"""Root HTTP server method
+		:return:
+		"""
+		account = self.get_account(True)
 
-		if not os.path.exists( 'accounts' ):
-			os.mkdir( 'accounts', 666 )
-		path = 'accounts/' + account
-		if not os.path.exists( path ):
-			os.mkdir( path, 666 )
-		raise cherrypy.HTTPRedirect( '/account', status=307 )
+		if not os.path.exists('accounts'):
+			os.mkdir('accounts', 666)
+		path = f"accounts/{account}"
+		if not os.path.exists(path):
+			os.mkdir(path, 666)
+		raise cherrypy.HTTPRedirect('/account', status=307)
 
-	# Login page, which performs a (visible) HTML redirection
 	@cherrypy.expose
-	def login(self):
-		return self.static_page( 'login.html' )
-	
-	# Identity provisioning by an IdP
-	@cherrypy.expose
-	def identity(self, username):
-		self.set_cookie( username )
-		raise cherrypy.HTTPRedirect( '/', status=307 )
+	def login(self) -> str:
+		"""Login page, which performs a (visible) HTML redirection
+		:return:
+		"""
+		return self.static_page('login.html')
 
-	# Expose account page
 	@cherrypy.expose
-	def account(self):
-		account = self.get_account( True )
-		return self.account_contents( account )
+	def identity(self, username: str):
+		"""Identity provisioning by an IdP
+		:param username:
+		:return:
+		"""
+		self.set_cookie(username)
+		raise cherrypy.HTTPRedirect('/', status=307)
 
-	# Get individual account image
 	@cherrypy.expose
-	def img(self, name):
-		account = self.get_account( True )
-		path = os.getcwd() + '/accounts/' + account + "/" + name
-		return cherrypy.lib.static.serve_file( path, content_type='jpg' );
+	def account(self) -> str:
+		"""Expose account page
+		:return:
+		"""
+		account = self.get_account(True)
+		return self.account_contents(account)
 
-	# Upload new image for an account
+	@cherrypy.expose
+	def img(self, name: str):
+		"""Get individual account image
+		:param name:
+		:return:
+		"""
+		account = self.get_account(True)
+		path = f"{os.getcwd()}/accounts/{account}/{name}"
+		return cherrypy.lib.static.serve_file(path, content_type='jpg')
+
 	@cherrypy.expose
 	def add(self, image):
-		name = self.random_name() 
-		account = self.get_account( False )
+		"""Upload new image for an account
+		:param image:
+		:return:
+		"""
+		name = self.random_name()
+		account = self.get_account(False)
 		if not account:
-			return self.static_page( 'login.html' );
+			return self.static_page('login.html')
 
-		path = Path( os.getcwd() + '/accounts/' + account + "/" + name )
+		path = Path(f"{os.getcwd()}/accounts/{account}/{name}")
 		m = hashlib.sha1()
-		with path.open( 'wb' ) as new_file:
+		with path.open('wb') as new_file:
 			while True:
-				data = image.file.read( 8192 )
+				data = image.file.read(8192)
 				if not data:
 					break
-				new_file.write( data )
-				m.update( data )
+				new_file.write(data)
+				m.update(data)
 
-		name = base64.urlsafe_b64encode( m.digest()[0:18] ).decode( 'utf8' )
-		new_path = Path( os.getcwd() + '/accounts/' + account + "/" + name )
+		name = base64.urlsafe_b64encode(m.digest()[0:18]).decode('utf8')
+		new_path = Path(f"{os.getcwd()}/accounts/{account}/{name}")
 		if not new_path.exists():
-		    path.rename( new_path )
+			path.rename(new_path)
 		else:
-		    path.unlink( missing_ok=True )
+			path.unlink(missing_ok=True)
 
-		return self.account_contents( account )
+		return self.account_contents(account)
 
-cherrypy.config.update( {'server.socket_host': '127.0.0.1',
-						 'server.socket_port': 8081 } )
-cherrypy.quickstart( SP() )
+
+cherrypy.config.update({'server.socket_host': '127.0.0.1',
+                        'server.socket_port': 8081})
+cherrypy.quickstart(SP())
